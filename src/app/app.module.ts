7a411135141381810ab121a46fc9400427a8190f
Arpit Narechania <arpitnarechania@gmail.com>
2025-06-21 17:43:14 +0800

    upgrade to angular 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from "@angular/core";
-import { HttpClientModule } from "@angular/common/http";
+import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
 import { HttpErrorHandler } from "./http-error-handler.service";
 import { FormsModule } from "@angular/forms";
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -14,26 +14,20 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { MainComponent } from "./pages/app/component";
 
-@NgModule({
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    HttpClientModule,
-    AppRoutingModule,
-    NgbModule,
-    CascadeSelectModule,
-    TooltipModule,
-    ColorPickerModule
-  ],
-  declarations: [
-    AppComponent,
-    MainComponent
-  ],
-  providers: [
-    HttpErrorHandler,
-    MainComponent
-  ],
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [
+        AppComponent,
+        MainComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        AppRoutingModule,
+        NgbModule,
+        CascadeSelectModule,
+        TooltipModule,
+        ColorPickerModule], providers: [
+        HttpErrorHandler,
+        MainComponent,
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AppModule {}
